be2d043ddd07f318699de4ead78899a26be12406
Sophie Teimournia <teimournia.sophie@laposte.net>
2021-03-30 18:13:14 +0200

    added graphic widget

---
+++ graphicwidget.h
@@ -0,0 +1,13 @@
+#ifndef GRAPHICWIDGET_H
+#define GRAPHICWIDGET_H
+
+#include <QGraphicsView>
+#include <QObject>
+
+class GraphicWidget : public QGraphicsView
+{
+public:
+    GraphicWidget();
+};
+
+#endif // GRAPHICWIDGET_H
